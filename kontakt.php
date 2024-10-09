69ee585ac0fec21e0670cfb1b2ff538c25951728
dsebert <146419300+dsebert@users.noreply.github.com>
2024-10-09 17:46:36 +0200

    Save kontakt.php.

--- kontakt.php
+++ kontakt.php
@@ -1,3 +1,23 @@
+<?php
+// Prüfen, ob das Formular gesendet wurde
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "dieunnuetzeseite@gmail.com";
+    $subject = "Neue Nachricht von Kontaktformular";
+    $body = "Name: $name\nE-Mail: $email\nNachricht:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "<p>Nachricht erfolgreich gesendet!</p>";
+    } else {
+        echo "<p>Fehler beim Senden der Nachricht.</p>";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -6,19 +26,18 @@
     <title>Kontaktformular</title>
 </head>
 <body>
-    <h1>Kontakt</h1>
-    <form action="https://formspree.io/f/YOUR_FORM_ID" method="POST">
+    <h1>Kontaktformular</h1>
+    <form action="kontakt.php" method="post">
         <label for="name">Name:</label><br>
         <input type="text" id="name" name="name" required><br><br>
-        
+
         <label for="email">E-Mail:</label><br>
         <input type="email" id="email" name="email" required><br><br>
-        
+
         <label for="message">Nachricht:</label><br>
         <textarea id="message" name="message" required></textarea><br><br>
-        
+
         <input type="submit" value="Absenden">
     </form>
 </body>
 </html>
-
